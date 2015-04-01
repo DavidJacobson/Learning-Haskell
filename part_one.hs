82953a66fa5be542b54f9aae07149fc570f572a5
David Jacobson <david@davidej.com>
2015-04-01 00:46:26 -0400

    set comprehensions and tuples

--- part_one.hs
+++ part_one.hs
@@ -9,8 +9,7 @@ doubleSmallNumber x = if x > 100
                       else x * 2
 
 -- Lists
-someInts = [1,3,4,5]
-basicPattern = [1..2]
+someInts = [1,3,4,5
 -- Functions on lists
 oneThroughTen = [2,3,4] ++ [5,6,7,8,9,10]
 1 : oneThroughTen  -- Missed the 1
@@ -24,4 +23,18 @@ length [1,2,3] -- length
 reverse [1,2,3] -- reverses the list
 
 null [] -- True, it's empty, same as list == []
--- 
+-- Checking indicies which dont exist can't be caught at compile time
+take 3 someInts
+drop 2 someInts -- Drops the first 2 elements
+6 `elem` [4,5,6] -- True
+-- There are more, but only so many are worth writing. eg: minimum, maximum
+
+--- Ranges
+[1..20]
+['a'..'z']
+take 10 (cycle [1..10])
+-- Time for sets
+doubles = [x*2 | x <- [1..10]]
+--         ^ 2x     ^ Get x from here
+doubledWithPred = [x*2 | x <- [1..10], x*2 >= 12]
+--tuples, (x,y) fst snd, and zip
